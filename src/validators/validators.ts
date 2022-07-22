2c81923f04226db1832e2cf4c2c8cd435d2c9c2e
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-07-22 11:17:16 +0200

    refactor: removed unnecessary checks object

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -7,26 +7,7 @@ import {
   validationResult,
 } from 'express-validator';
 import express from 'express';
-
-const checks: { [key: string]: ValidationChain } = {
-  id: param('id').isMongoId(),
-  title: body('title')
-    .isLength({ min: 3, max: 20 })
-    .trim()
-    .stripLow(false)
-    .escape(),
-  body: body('body').isLength({ min: 20, max: 1000 }).trim().escape(),
-  query: param('query').isAlpha().trim(),
-  page: query('page').optional().isInt().toInt(),
-  limit: query('limit').optional().isInt().toInt(),
-  username: body('username')
-    .isAlphanumeric()
-    .isLength({ min: 3, max: 15 })
-    .trim(),
-  password: body('password').isLength({ min: 6, max: 200 }),
-  token: header('authorization').isJWT(),
-};
-
+//unnecessary
 export const validate = (validations: ValidationChain[]) => {
   return async (
     req: express.Request,
