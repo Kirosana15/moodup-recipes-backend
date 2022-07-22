79b001fabb5007f47e894a809d052b15aaf0824b
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-07-22 12:59:29 +0200

    feature: add validations

--- src/validators/recipeValidators.ts
+++ src/validators/recipeValidators.ts
@@ -4,11 +4,14 @@ const validateId = param('id').isMongoId();
 const validatePage = query('page').optional().isInt().toInt();
 const validateLimit = query('limit').optional().isInt().toInt();
 const validateTitle = body('title')
+  .optional()
   .isLength({ min: 3, max: 20 })
   .trim()
   .stripLow(false)
   .escape();
+const validateImageUrl = body('imageUrl').optional().isURL().escape();
 const validateBody = body('body')
+  .optional()
   .isLength({ min: 20, max: 1000 })
   .trim()
   .escape();
@@ -26,11 +29,13 @@ export const validateGetAllRecipes: ValidationChain[] = [
 
 export const validateCreateRecipe: ValidationChain[] = [
   validateTitle,
+  validateImageUrl,
   validateBody,
 ];
 
 export const validateUpdateRecipe: ValidationChain[] = [
   validateId,
+  validateImageUrl,
   validateBody,
 ];
 
