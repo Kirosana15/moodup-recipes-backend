fcbfcba10de85ec13a7bf4889bfd2875649207ef
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-07-27 15:10:30 +0200

    add userService.createUser tests

--- src/tests/userService/userService.createUser.test.ts
+++ src/tests/userService/userService.createUser.test.ts
@@ -1,10 +1,11 @@
 import UserService from '../../services/userService';
 import { User } from '../../models/userModel';
 import mongoose from 'mongoose';
+import { IUser } from '../../interfaces/user';
 
 const userService = new UserService();
 
-describe('Testing userService', () => {
+describe('Testing createUser', () => {
   beforeAll(async () => {
     try {
       const dbUri = 'mongodb://localhost:27017';
@@ -26,13 +27,37 @@ describe('Testing userService', () => {
     }
   });
 
-  test('createUser returns new user', async () => {
+  test('save new User in a database', async () => {
     const username = 'jestuser';
     const password = 'jesttest';
     await userService.createUser(username, password);
-    const user = await User.findOne({ username });
+    const user = <IUser>await User.findOne({ username });
     expect(user).toBeDefined();
     expect(user.username).toBe(username);
     expect(user.password).toBe(password);
+  });
+  describe('error thrown when', () => {
+    test('username already exists', async () => {
+      const username = 'dupeuser';
+      const password = 'jesttest';
+      await userService.createUser(username, password);
+      await expect(
+        userService.createUser(username, password)
+      ).rejects.toThrow();
+    });
+    test('no username provided', async () => {
+      const username = '';
+      const password = 'jesttest';
+      await expect(
+        userService.createUser(username, password)
+      ).rejects.toThrow();
+    });
+    test('no password provided', async () => {
+      const username = 'riskyuser';
+      const password = '';
+      await expect(
+        userService.createUser(username, password)
+      ).rejects.toThrow();
+    });
   });
 });
