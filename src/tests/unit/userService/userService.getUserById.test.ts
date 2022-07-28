1c7c588a6f84a23fd16c674fb4147ed14700d8b4
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-07-28 13:15:17 +0200

    add userService.getUserById() tests

---
+++ src/tests/unit/userService/userService.getUserById.test.ts
@@ -0,0 +1,39 @@
+import { mockId, mockPassword, mockUsername } from '../../mocks/mockUser';
+import UserService from '../../../services/userService';
+import {
+  clearAllCollections,
+  closeConnection,
+  connectToDb,
+} from '../../consts';
+import { User } from '../../../models/userModel';
+
+const userService = new UserService();
+
+describe('Testing getUserById', () => {
+  beforeAll(async () => {
+    connectToDb('getUserById-test');
+  });
+  afterEach(async () => {
+    clearAllCollections();
+  });
+  afterAll(() => {
+    closeConnection();
+  });
+  test("doesn't return user when id doesn't exist", async () => {
+    const user = await userService.getUserById(mockId);
+    expect(user).toBeNull();
+  });
+  test("throws when id isn't provided", () => {
+    expect(userService.getUserById('')).rejects.toThrow();
+  });
+  test('fetch User by id', async () => {
+    const newUser = await new User({
+      username: mockUsername,
+      password: mockPassword,
+    }).save();
+    const user = await userService.getUserById(newUser.id);
+    expect(user).toBeDefined();
+    expect(user?.username).toBe(mockUsername);
+    expect(user?.password).toBe(mockPassword);
+  });
+});
