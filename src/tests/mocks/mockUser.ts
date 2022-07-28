57365e4e8e151cb46499e1904234f027f8b6987e
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-07-28 15:41:53 +0200

    add userService.getAllUsers() tests

--- src/tests/mocks/mockUser.ts
+++ src/tests/mocks/mockUser.ts
@@ -8,8 +8,10 @@ export const mockToken = faker.datatype.string(30);
 export class mockUser {
   public username: string;
   public password: string;
+  public createdAt: Date;
   constructor() {
     this.username = faker.internet.userName();
     this.password = faker.internet.password();
+    this.createdAt = faker.date.past();
   }
 }
