8ba1af355170194e64f5d4a617027c708b2543f2
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-07-29 12:24:06 +0200

    add userService.generateToken() tests

--- src/tests/consts.ts
+++ src/tests/consts.ts
@@ -3,6 +3,7 @@ import mongoose, { Model } from 'mongoose';
 export const DB_URI = 'mongodb://localhost:27017';
 export const CONN_ERR = 'DB connect error';
 export const DC_ERR = 'DB disconnect error';
+export const MATCH_JWT = /^(?:[\w-]+\.){2}[\w-]+$/; //matches a JWT token
 
 export const connectToDb = async (dbName: string) => {
   try {
