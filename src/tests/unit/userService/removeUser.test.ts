4131a9c3367b4169772bdc6c348e2f6b573f9837
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-01 09:07:27 +0200

    refactor code

---
+++ src/tests/unit/userService/removeUser.test.ts
@@ -0,0 +1,30 @@
+import { mockId, saveUser, saveUsers } from '../../mocks/mockUser';
+import UserService from '../../../services/userService';
+import { User } from '../../../models/userModel';
+import { setupTests } from '../../setupTests';
+
+const userService = new UserService();
+
+setupTests('removeUser', () => {
+  test('returns removed user', async () => {
+    const newUser = await saveUser();
+    const user = await userService.removeUser(newUser.id);
+    expect(user).toBeDefined();
+    expect(user?.username).toBe(newUser.username);
+    expect(user?.password).toBe(newUser.password);
+  });
+  test('removes user from database', async () => {
+    const newUser = await saveUser();
+    await userService.removeUser(newUser.id);
+    expect(await User.findById(newUser.id).exec()).toBeNull();
+  });
+  test("doesn't return user if id doesn't exist", async () => {
+    const user = await userService.removeUser(mockId);
+    expect(user).toBeNull();
+  });
+  test("throws and doesn't remove any users if id is not provided", async () => {
+    saveUsers(2);
+    await expect(userService.removeUser('')).rejects.toThrow();
+    expect(await User.countDocuments({})).toBe(2);
+  });
+});
