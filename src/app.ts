6b31d31bc3a07072e67b2106976b9aeb6177bfcc
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-04 08:48:12 +0200

    fix missing return; remove comments from app

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
 import 'dotenv/config';
-import express, { Application } from 'express';
+import express from 'express';
 import morgan from 'morgan';
 
 import userRouter from './routes/userRouter';
 import recipeRouter from './routes/recipeRouter';
 
-const app: Application = express();
+const app = express();
 
-//adding middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.static('public'));
 
-//routing
 app.use(userRouter);
 app.use(recipeRouter);
 
