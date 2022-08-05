11c736b8deede7a37f7f46a29aa9b8bd7df926fa
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-05 16:49:21 +0200

    add GET /user/{id} tests

--- src/tests/e2e/users.id.get.test.ts
+++ src/tests/e2e/users.id.get.test.ts
@@ -1,7 +1,32 @@
-import { setupTests } from '../setupTests';
+import { setupE2E } from '../setupTests';
 import request from 'supertest';
 import app from '../../app';
-import { mockValidToken, generateToken } from '../mockObjects/mockToken';
 import { StatusCodes } from 'http-status-codes';
 import { userService } from '../../services/userService';
-import authController from '../../controllers/authController';
+import { mockGetUserById } from '../mockObjects/mockUserService';
+import { mockId } from '../mockObjects/mockUser';
+import { mockValidToken } from '../mockObjects/mockToken';
+
+setupE2E('usersidget-e2e', () => {
+  const getUserByIdSpy = jest.spyOn(userService, 'getUserById').mockImplementation(mockGetUserById);
+  describe('GET /users/{id} should', () => {
+    test(`respond with ${StatusCodes.INTERNAL_SERVER_ERROR} when error is thrown`, async () => {
+      getUserByIdSpy.mockRejectedValueOnce('test');
+      const res = await request(app).get(`/users/${mockId}`).set('Authorization', `Bearer ${mockValidToken}`);
+      expect(res.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+    });
+
+    test(`respond with ${StatusCodes.NOT_FOUND} when user does not exist`, async () => {
+      getUserByIdSpy.mockResolvedValueOnce(null);
+      const res = await request(app).get(`/users/${mockId}`).set('Authorization', `Bearer ${mockValidToken}`);
+      expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
+      expect(res.text).toEqual('User not found');
+    });
+
+    test(`respond with ${StatusCodes.OK} and user data`, async () => {
+      const res = await request(app).get(`/users/${mockId}`).set('Authorization', `Bearer ${mockValidToken}`);
+      expect(res.statusCode).toBe(StatusCodes.OK);
+      expect(res.body.username).toBeDefined();
+    });
+  });
+});
