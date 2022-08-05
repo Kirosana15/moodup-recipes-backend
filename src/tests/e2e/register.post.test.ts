ead76a9b6bc2f39b2a872cec81c5d2cd4f29269b
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-05 16:12:05 +0200

    fix POST /register tests

--- src/tests/e2e/register.post.test.ts
+++ src/tests/e2e/register.post.test.ts
@@ -6,21 +6,19 @@ import { setupTests } from '../setupTests';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import bcrypt from 'bcrypt';
 import { userService } from '../../services/userService';
+import { mockCreateUser } from '../mockObjects/mockUserService';
 
 setupTests('register-e2e', () => {
+  const createUserSpy = jest.spyOn(userService, 'createUser').mockImplementation(mockCreateUser);
   describe('POST /register should respond', () => {
-    const createUserSpy = jest.spyOn(userService, 'createUser').mockImplementation((): any => {
-      return Promise.resolve({ username: mockUsername, password: 'hash' });
-    });
-
-    test('with created user with hashed password', async () => {
+    test('with created user', async () => {
       const res = await request(app).post('/register').send({ username: mockUsername, password: mockPassword });
 
       expect(res.statusCode).toBe(StatusCodes.CREATED);
       expect(bcrypt.hash).toBeCalledTimes(1);
-      expect(createUserSpy).toBeCalledTimes(1);
+      expect(userService.createUser).toBeCalledTimes(1);
       expect(res.body.username).toBe(mockUsername);
-      expect(res.body.password).toBe('hash');
+      expect(res.body._id).toBeDefined();
     });
 
     test('with 400 when username is not provided', async () => {
@@ -40,9 +38,7 @@ setupTests('register-e2e', () => {
     });
 
     test('with 400 and error message "User already exists" when username already in database', async () => {
-      createUserSpy.mockImplementationOnce(() => {
-        return Promise.reject({ code: 11000 });
-      });
+      createUserSpy.mockRejectedValueOnce({ code: 11000 });
       const res = await request(app).post('/register').send({ username: mockUsername, password: mockPassword });
 
       expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
@@ -51,14 +47,11 @@ setupTests('register-e2e', () => {
     });
 
     test('with INTERNAL_SERVER_ERROR when userService throws an error', async () => {
-      createUserSpy.mockImplementation(() => {
-        throw { code: 1 };
-      });
+      createUserSpy.mockRejectedValueOnce({ code: 1 });
       const errRes = await request(app).post('/register').send({ username: mockUsername, password: mockPassword });
 
       expect(errRes.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
       expect(errRes.text).toBe(ReasonPhrases.INTERNAL_SERVER_ERROR);
-      expect(createUserSpy).toThrow();
     });
 
     test(`with ${StatusCodes.BAD_REQUEST} when username validation fails`, async () => {
