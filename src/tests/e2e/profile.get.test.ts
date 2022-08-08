b646335efbb7f6516376ef01ffacc65e6218ca49
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-08 10:58:07 +0200

    add GET /profile tests

---
+++ src/tests/e2e/profile.get.test.ts
@@ -0,0 +1,22 @@
+import { setupE2E } from '../setupTests';
+import request from 'supertest';
+import app from '../../app';
+import { generateToken } from '../mockObjects/mockToken';
+import { mockUsername } from '../mockObjects/mockUser';
+import { StatusCodes } from 'http-status-codes';
+
+setupE2E('profile-get-e2e', () => {
+  test(`should respond with ${StatusCodes.OK} and user information for logged in user`, async () => {
+    const res = await request(app)
+      .get('/profile')
+      .set('Authorization', `Bearer ${generateToken({ username: mockUsername, isAdmin: false })}`);
+    expect(res.status).toBe(StatusCodes.OK);
+    expect(res.body.username).toBe(mockUsername);
+    expect(res.body.isAdmin).toBeFalsy();
+  });
+
+  test(`should respond with ${StatusCodes.UNAUTHORIZED} if user is not logged in`, async () => {
+    const res = await request(app).get('/profile');
+    expect(res.status).toBe(StatusCodes.UNAUTHORIZED);
+  });
+});
