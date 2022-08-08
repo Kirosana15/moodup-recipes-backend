2c5038745cb0827ef507905122bb529730939946
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-08 14:49:14 +0200

    save endpoint to a constant

--- src/tests/e2e/profile.get.test.ts
+++ src/tests/e2e/profile.get.test.ts
@@ -5,10 +5,12 @@ import { generateToken } from '../mockObjects/mockToken';
 import { mockUsername } from '../mockObjects/mockUser';
 import { StatusCodes } from 'http-status-codes';
 
+const ENDPOINT = '/profile';
+
 setupE2E('profile-get-e2e', () => {
   test(`should respond with ${StatusCodes.OK} and user information for logged in user`, async () => {
     const res = await request(app)
-      .get('/profile')
+      .get(ENDPOINT)
       .set('Authorization', `Bearer ${generateToken({ username: mockUsername, isAdmin: false })}`);
     expect(res.status).toBe(StatusCodes.OK);
     expect(res.body.username).toBe(mockUsername);
@@ -16,7 +18,7 @@ setupE2E('profile-get-e2e', () => {
   });
 
   test(`should respond with ${StatusCodes.UNAUTHORIZED} if user is not logged in`, async () => {
-    const res = await request(app).get('/profile');
+    const res = await request(app).get(ENDPOINT);
     expect(res.status).toBe(StatusCodes.UNAUTHORIZED);
   });
 });
