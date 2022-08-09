0b229a183af68a4271fb7857ebe5a71f66ba6c24
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-09 11:24:24 +0200

    improve readability

--- src/tests/e2e/users.id.delete.test.ts
+++ src/tests/e2e/users.id.delete.test.ts
@@ -16,25 +16,29 @@ setupE2E('usersiddelete-e2e', () => {
     test(`respond with ${StatusCodes.INTERNAL_SERVER_ERROR} when error is thrown`, async () => {
       removeUserSpy.mockRejectedValueOnce('test');
       const res = await request(app).delete(ENDPOINT).set('Authorization', TOKEN);
+
       expect(res.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
     });
 
     test(`respond with ${StatusCodes.NOT_FOUND} when id does not exist`, async () => {
       removeUserSpy.mockResolvedValueOnce(null);
       const res = await request(app).delete(ENDPOINT).set('Authorization', TOKEN);
+
       expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
     });
 
-    test(`respond with ${StatusCodes.FORBIDDEN} when user is not an admin or user to be deleted`, async () => {
+    test(`respond with ${StatusCodes.FORBIDDEN} when user is not an admin and trying to delete another user`, async () => {
       const res = await request(app)
         .delete(`/users/${generateMockId()}`)
         .set('Authorization', `Bearer ${generateToken({ isAdmin: false, id: mockId })}`);
+
       expect(res.statusCode).toBe(StatusCodes.FORBIDDEN);
     });
     test(`respond with ${StatusCodes.OK} and deleted user if user is an admin`, async () => {
       const res = await request(app)
         .delete(`/users/${generateMockId()}`)
         .set('Authorization', `Bearer ${generateToken({ isAdmin: true, id: mockId })}`);
+
       expect(res.statusCode).toBe(StatusCodes.OK);
       expect(res.body.username).toBeDefined();
     });
@@ -42,10 +46,11 @@ setupE2E('usersiddelete-e2e', () => {
       const res = await request(app)
         .delete(ENDPOINT)
         .set('Authorization', `Bearer ${generateToken({ isAdmin: false, id: mockId })}`);
+
       expect(res.statusCode).toBe(StatusCodes.OK);
       expect(res.body.username).toBeDefined();
     });
-    test(`respond with ${StatusCodes.BAD_REQUEST} if :id is not valid mongoId`, async () => {
+    test(`respond with ${StatusCodes.BAD_REQUEST} if id is not valid mongoId`, async () => {
       const res = await request(app)
         .delete(`/users/1`)
         .set('Authorization', `Bearer ${generateToken({ isAdmin: false, id: mockId })}`);
