bad94d88f3bc5df66c2370274f1d3db1acb930e3
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-09 11:44:12 +0200

    do not escape title, body and imageUrl of a recipe

--- src/validators/recipeValidators.ts
+++ src/validators/recipeValidators.ts
@@ -3,46 +3,25 @@ import { query, param, ValidationChain, body } from 'express-validator';
 const validateId = param('id').isMongoId();
 const validatePage = query('page').optional().isInt().toInt();
 const validateLimit = query('limit').optional().isInt().toInt();
-const validateTitle = body('title')
-  .isLength({ min: 3, max: 20 })
-  .trim()
-  .stripLow(false)
-  .escape();
-const validateImageUrl = body('imageUrl').optional().isURL().escape();
-const validateBody = body('body')
-  .isLength({ min: 20, max: 1000 })
-  .trim()
-  .escape();
+const validateTitle = body('title').isLength({ min: 3, max: 20 }).trim().stripLow(false);
+const validateImageUrl = body('imageUrl').optional().isURL();
+const validateBody = body('body').isLength({ min: 20, max: 1000 }).trim();
 const validateQuery = param('query').isAlpha().trim();
 
-export const validateGetRecipes: ValidationChain[] = [
-  validatePage,
-  validateLimit,
-];
+export const validateGetRecipes: ValidationChain[] = [validatePage, validateLimit];
 
-export const validateGetAllRecipes: ValidationChain[] = [
-  validatePage,
-  validateLimit,
-];
+export const validateGetAllRecipes: ValidationChain[] = [validatePage, validateLimit];
 
-export const validateCreateRecipe: ValidationChain[] = [
-  validateTitle,
-  validateImageUrl,
-  validateBody,
-];
+export const validateCreateRecipe: ValidationChain[] = [validateTitle, validateImageUrl, validateBody];
 
 export const validateUpdateRecipe: ValidationChain[] = [
   validateId,
-  validateTitle,
+  validateTitle.optional(),
   validateImageUrl,
-  validateBody,
+  validateBody.optional(),
 ];
 
-export const validateSearchRecipes: ValidationChain[] = [
-  validateQuery,
-  validatePage,
-  validateLimit,
-];
+export const validateSearchRecipes: ValidationChain[] = [validateQuery, validatePage, validateLimit];
 
 export const validateRemoveRecipe: ValidationChain[] = [validateId];
 export const validateGetRecipe: ValidationChain[] = [validateId];
