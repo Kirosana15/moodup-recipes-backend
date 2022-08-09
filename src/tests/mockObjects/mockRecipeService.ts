906698d93a766c36d67de9883d4ed9bf0cb7cae3
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-09 11:44:12 +0200

    allow customizing mockGetRecipe and mockUpdateRecipe

--- src/tests/mockObjects/mockRecipeService.ts
+++ src/tests/mockObjects/mockRecipeService.ts
@@ -1,8 +1,8 @@
-import { generateRecipe, generateRecipes } from './mockRecipe';
+import { mockId, generateRecipe, generateRecipes } from './mockRecipe';
 
 export const mockCreateRecipe = jest.fn().mockResolvedValue(generateRecipe());
 
-export const mockGetRecipe = jest.fn().mockResolvedValue(generateRecipe());
+export const mockGetRecipe = jest.fn().mockImplementation((id = mockId) => Promise.resolve(generateRecipe(id)));
 
 export const mockGetAllRecipes = jest.fn().mockImplementation((limit = 10) => Promise.resolve(generateRecipes(limit)));
 
@@ -10,8 +10,11 @@ export const mockGetRecipesByOwner = jest
   .fn()
   .mockImplementation((limit = 10) => Promise.resolve(generateRecipes(limit)));
 
-export const mockUpdateRecipe = jest.fn().mockResolvedValue(generateRecipe());
-
+export const mockUpdateRecipe = jest
+  .fn()
+  .mockImplementation((id, title?, imageUrl?, body?) =>
+    Promise.resolve({ id, ...generateRecipe(undefined, title, imageUrl, body) }),
+  );
 export const mockRemoveRecipe = jest.fn().mockResolvedValue(generateRecipe());
 
 export const mockGetRecipesByTitle = jest
