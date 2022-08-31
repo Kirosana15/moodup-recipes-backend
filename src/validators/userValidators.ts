10c63c51e2d614dddfd48f1a5e69f87d60306dc8
KamilGrygorowicz <grygorowiczk@protonmail.com>
2022-08-31 10:01:50 +0200

    fix: :bug: fix server crash when password is a number and not a string

--- src/validators/userValidators.ts
+++ src/validators/userValidators.ts
@@ -1,17 +1,16 @@
-import { query, param, ValidationChain, body, header, CustomValidator } from 'express-validator';
+import { CustomValidator, ValidationChain, body, header, param, query } from 'express-validator';
 
 const validateId = param('id').isMongoId();
 const validatePage = query('page').optional().isInt().toInt();
 const validateLimit = query('limit').optional().isInt().toInt();
 const validateUsername = body('username').isAlphanumeric().isLength({ min: 3, max: 15 }).trim();
-const validatePassword = body('password').isLength({ min: 6, max: 200 });
+const validatePassword = body('password').isLength({ min: 6, max: 72 }).isString();
 
 const validateBasic: CustomValidator = (auth: string) => {
   const basicString = auth.split(' ')[1];
   const [username, password] = Buffer.from(basicString, 'base64').toString('utf-8').split(':');
   const isValidUsername = username.length >= 3 && username.length < 15 && username.match(/^[\w]+$/);
-  console.log(password.length);
-  const isValidPassword = password.length >= 6 && password.length < 200;
+  const isValidPassword = password.length >= 6 && password.length < 72;
   return isValidPassword && isValidUsername;
 };
 
